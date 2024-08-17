35f4e0d1dbd8b6ded59e2d592cf2e91b53427db0
dongmin7208 <sayg3o3d@gmail.com>
2024-08-17 15:33:11 +0900

    ADD: prewview id

--- src/app/preview/[id]/page.tsx
+++ src/app/preview/[id]/page.tsx
@@ -2,12 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { useEditorStore } from "@/store/editorStore";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 const PreviewPage: React.FC = () => {
   const { editorData } = useEditorStore();
   console.log("editorData", editorData);
   const router = useRouter();
+  const uid = useParams().id;
+  console.log("uid", uid);
 
   const handleEdit = () => {
     router.push("/editor");
@@ -18,7 +20,9 @@ const PreviewPage: React.FC = () => {
       <Button className="absolute top-26 right-4" onClick={handleEdit}>
         수정하기
       </Button>
+      <p>({uid})</p>
       <h1 className="pb-10 text-2xl font-bold">Preview</h1>
+      <div className="border-b-2 border-gray-200 mb-4"></div>
       <div dangerouslySetInnerHTML={{ __html: editorData }} />
     </div>
   );
