e8e51d2d70bcff6f8d41ace1696cdc399b2a7032
dongmin7208 <sayg3o3d@gmail.com>
2024-08-18 22:17:12 +0900

    ADD: Preview-Mobile

--- src/app/preview/[id]/page.tsx
+++ src/app/preview/[id]/page.tsx
@@ -1,29 +1,48 @@
 "use client";
-
 import { Button } from "@/components/ui/button";
 import { useEditorStore } from "@/store/editorStore";
 import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
 
 const PreviewPage: React.FC = () => {
   const { editorData } = useEditorStore();
-  console.log("editorData", editorData);
   const router = useRouter();
   const uid = useParams().id;
-  console.log("uid", uid);
+  const [isMobileView, setIsMobileView] = useState(false);
 
   const handleEdit = () => {
     router.push("/editor");
   };
 
+  const toggleViewMode = () => {
+    setIsMobileView(!isMobileView);
+  };
+
   return (
-    <div>
-      <Button className="absolute top-26 right-4" onClick={handleEdit}>
-        수정하기
-      </Button>
+    <div className="relative pb-20">
+      <div className="flex justify-end space-x-2">
+        <Button onClick={toggleViewMode}>
+          {!isMobileView ? "PC" : "Mobile"}
+        </Button>
+        <Button onClick={handleEdit}>수정하기</Button>
+      </div>
       <p>({uid})</p>
       <h1 className="pb-10 text-2xl font-bold">Preview</h1>
       <div className="border-b-2 border-gray-200 mb-4"></div>
-      <div dangerouslySetInnerHTML={{ __html: editorData }} />
+
+      {isMobileView ? (
+        <div className="flex justify-center">
+          <div className="relative w-[375px] h-[812px] bg-black rounded-[40px] overflow-hidden shadow-lg border-8 border-black">
+            <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-[210px] h-[30px] bg-black rounded-b-[15px]"></div>
+            <div className="absolute top-[40px] bottom-[40px] left-0 right-0 bg-white overflow-y-auto p-4">
+              <div dangerouslySetInnerHTML={{ __html: editorData }} />
+            </div>
+            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-[130px] h-[5px] bg-gray-400 rounded-full"></div>
+          </div>
+        </div>
+      ) : (
+        <div dangerouslySetInnerHTML={{ __html: editorData }} />
+      )}
     </div>
   );
 };
