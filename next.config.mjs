61c8467f346414b1472375e865260617edd20262
dongmin7208 <sayg3o3d@gmail.com>
2024-10-29 22:54:26 +0900

    FIX:https

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,25 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
+        protocol: 'http',
         hostname: 'dazzi-test-editor.s3.ap-northeast-1.amazonaws.com',
         pathname: '/**',
       },
     ],
+  },
+  async rewrites() {
+    return [
+      // 로그인 관련 요청 예외 설정
+      {
+        source: '/api/auth/:path*',
+        destination: '/api/auth/:path*', // 자체 Next.js API 라우트로 전달
+      },
+      // 그 외의 모든 API 요청은 백엔드로 프록시
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`, // 환경 변수를 이용한 프록시
+      },
+    ];
   },
 };
 
--- src/app/api/categories.ts
+++ src/app/api/categories.ts
@@ -1,10 +1,16 @@
+import axios from "axios";
+import https from "https";
 import { AddCategoryRequest, Category } from "../types/category";
 import axiosInstance from "./axiosInstance";
 
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 // 카테고리 리스트 가져오기
 export const fetchCategories = async (): Promise<Category[]> => {
-  console.log("API URL:??", process.env.NEXT_PUBLIC_API_BASE_URL);
-  const response = await axiosInstance.get("/category/list");
+  console.log("API URL:무엇?", process.env.NEXT_PUBLIC_API_BASE_URL);
+  const response = await axios.get("/category/list", { httpsAgent: agent });
   console.log("category/list:", response);
 
   return response.data.data;
