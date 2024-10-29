8053df7e1338495f9b414ddb6a4693cbda0b5b0e
dongmin7208 <sayg3o3d@gmail.com>
2024-10-29 23:58:02 +0900

    FIX:https

--- next.config.mjs
+++ next.config.mjs
@@ -4,20 +4,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
+        protocol: 'https',
         hostname: 'dazzi-test-editor.s3.ap-northeast-1.amazonaws.com',
         pathname: '/**',
       },
     ],
   },
-  // async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/auth/:path*',
-  //       destination: '/api/auth/:path*', // 자체 Next.js API 라우트로 전달
-  //     },
-  //   ];
-  // },
 };
 
 export default nextConfig;
